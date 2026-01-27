f6de924f811b1ad6269958e9fdd74f4de5983146
pkstoy13 <pkstoyanov1@gmail.com>
2026-01-26 19:15:08 -0500

    made bash script for installing Java and things on Azure VM

---
+++ install_backend.sh
@@ -0,0 +1,35 @@
+# exit if any command fails
+set -e
+
+# update packages
+echo "[INFO] Updating system packages..."
+sudo apt update -y
+sudo apt upgrade -y
+
+# install backend packages
+echo "Installing Java and other required packages"
+sudo apt install -y openjdk-25-jdk maven git
+
+# install docker
+echo "installing Docker"
+sudo apt install -y docker.io
+sudo systemctl enable --now docker
+
+# set up backend directory
+BACKEND_DIR="/opt/tfSimulator/backend"
+echo "setting up backend directory at $BACKEND_DIR"
+sudo mkdir -p $BACKEND_DIR
+sudo chown $USER:$USER $BACKEND_DIR
+
+# clone tfSimulator repo from github
+echo "cloning tfSimulator repo"
+git clone https://github.com/pkstoy13/tfSimulator.git $BACKEND_DIR
+
+# run Spring boot app
+echo "running Spring Boot App"
+cd $BACKEND_DIR/api
+mvn clean package
+java -jar target/*.jar &
+
+echo "backend set up and running"
+
